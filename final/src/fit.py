c40b11e7d7ec36fbbfb1acf57a192e71b012ba84
Alejandro Rodríguez Salamanca <alexrs95@gmail.com>
2017-05-24 14:32:21 +0200

    Skeleton for fit. Let's read again the papers

--- final/src/fit.py
+++ final/src/fit.py
@@ -2,8 +2,7 @@
 Authors: Alejandro Rodriguez, Fernando Collado
 """
 
-# maximum allowed iterations to fit the model
-MAX_ITER = 40
+MAX_ITER = 40 # maximum allowed iterations to fit the model
 SEARCH_POINTS = 15 # Number of points to search when fitting the image
 
 class Fit(object):
@@ -11,6 +10,14 @@ class Fit(object):
     TODO
     """
 
-    def __init__(self, initial_fit, img):
+    def __init__(self, initial_fit, test_img):
         self._initial_fit = initial_fit
-        self._img = img
+        self._test_img = test_img
+
+    def _fit(self):
+        pass
+    
+    def _find(self):
+
+    def _update(self):
+        
